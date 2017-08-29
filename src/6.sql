705482b4f723843c4d95231560155481a32ff929
AlexPtrv <2581653@ukr.net>
2017-08-29 12:53:26 +0300

    Update 6.sql

--- src/6.sql
+++ src/6.sql
@@ -1,10 +1,16 @@
 USE `modul1`;
+
 SELECT
-projects.project_name,
-avg(developers.salary) as avg_salary
-FROM developers
-INNER JOIN
-   projects_developers ON developers.id=projects_developers.developer_id
-INNER JOIN
- projects ON projects.id= projects_developers.project_id
-WHERE projects.cost= (SELECT min(cost) FROM projects)
+  lpd.project_id,
+  avg(d.salary) AS avg_developer_salary
+FROM developers d
+  JOIN (SELECT *
+        FROM projects_developers pd
+          JOIN (SELECT *
+                FROM projects p2
+                  JOIN (SELECT min(p.cost) AS min_cost
+                        FROM projects p) AS p3
+                    ON p2.cost = p3.min_cost) AS p4
+            ON pd.project_id = p4.id) AS lpd
+    ON d.id = lpd.developer_id
+GROUP BY lpd.project_id;
