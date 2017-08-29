5a4f0356d5c760c6662b46a10c69eef0ad93496a
AlexPtrv <2581653@ukr.net>
2017-08-29 12:52:23 +0300

    Update 4.sql

--- src/4.sql
+++ src/4.sql
@@ -1,2 +1,15 @@
-USE `modul1`;
-ALTER TABLE projects ADD cost DOUBLE ;
+
+ALTER TABLE projects
+  ADD cost FLOAT;
+
+UPDATE projects
+SET cost = 80000
+WHERE id = 1;
+
+UPDATE projects
+SET cost = 50000
+WHERE id = 2;
+
+UPDATE projects
+SET cost = 20000
+WHERE id = 3;
