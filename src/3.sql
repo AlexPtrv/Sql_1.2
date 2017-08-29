6b4d9732d146543957c652efba26ccef245809d3
AlexPtrv <2581653@ukr.net>
2017-08-29 12:52:01 +0300

    Update 3.sql

--- src/3.sql
+++ src/3.sql
@@ -1,10 +1,5 @@
 USE `modul1`;
-SELECT sum(developers.salary) as Salary_JAVA_developers
-FROM developers
-  INNER JOIN developers_skills
-  INNER JOIN skills
-WHERE developers.id=developers_skills.developer_id
-  AND skills.id=developers_skills.skill_id
-      AND skills.skill_name LIKE 'JAVA'
+SELECT sum(salary) AS total_salary FROM
+  developers;
 
 
