111de0dbc14289aef2fd3be18dbe8e81bf6a20f5
AlexPtrv <2581653@ukr.net>
2017-08-29 12:55:20 +0300

    Update 4.sql

--- src/4.sql
+++ src/4.sql
@@ -3,13 +3,13 @@ ALTER TABLE projects
   ADD cost FLOAT;
 
 UPDATE projects
-SET cost = 80000
+SET cost = 120000
 WHERE id = 1;
 
 UPDATE projects
-SET cost = 50000
+SET cost = 70000
 WHERE id = 2;
 
 UPDATE projects
-SET cost = 20000
+SET cost = 30000
 WHERE id = 3;
