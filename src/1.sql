7f04299e20d1f5811fcf057016fa9708c2279c1d
AlexPtrv <2581653@ukr.net>
2017-08-29 12:53:52 +0300

    Update 1.sql

--- src/1.sql
+++ src/1.sql
@@ -1,8 +1,19 @@
 USE `modul1`;
-ALTER TABLE developers ADD salary DOUBLE ;
-UPDATE developers SET salary=14000 where id=1;
-UPDATE developers SET salary=140000 where id=2;
-UPDATE developers SET salary=11700 where id=3;
-UPDATE developers SET salary=170000 where id=4;
-UPDATE developers SET salary=100800 where id=5;
-SELECT * FROM developers;
+ALTER TABLE developers
+  ADD salary FLOAT;
+
+UPDATE developers
+SET salary = 1900.21
+WHERE id = 1;
+
+UPDATE developers
+SET salary = 5000.5
+WHERE id = 2;
+
+UPDATE developers
+SET salary = 3000
+WHERE id = 3;
+
+UPDATE developers
+SET salary = 1000
+WHERE id = 4;
